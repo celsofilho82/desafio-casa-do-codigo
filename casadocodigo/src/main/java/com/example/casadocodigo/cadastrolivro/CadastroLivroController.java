646731743057f3540dfe2cc947390e1ebfd21961
Celso Ribeiro <celsofilho82@gmail.com>
2021-01-25 22:18:14 -0300

    Implementação do endpoint detalhes de um livro

--- casadocodigo/src/main/java/com/example/casadocodigo/cadastrolivro/CadastroLivroController.java
+++ casadocodigo/src/main/java/com/example/casadocodigo/cadastrolivro/CadastroLivroController.java
@@ -1,6 +1,7 @@
 package com.example.casadocodigo.cadastrolivro;
 
 import java.util.List;
+import java.util.Optional;
 
 import javax.validation.Valid;
 
@@ -9,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.casadocodigo.cadastroautor.AutorRepository;
 import com.example.casadocodigo.cadastrocategoria.CategoriaRepository;
+import com.example.casadocodigo.detalheslivro.LivroDetalhes;
 import com.example.casadocodigo.detalheslivro.LivroResponse;
 
 @RestController
@@ -54,7 +57,19 @@ public class CadastroLivroController {
 	@GetMapping
 	public List<LivroResponse> listarLivros() {
 		List<Livro> livros = livroRepository.findAll();
-		
+
 		return LivroResponse.convert(livros);
+	}
+
+	@GetMapping(path = "/{id}")
+	public ResponseEntity<LivroDetalhes> mostrarLivro(@PathVariable Long id) {
+		Optional<Livro> possivelLivro = livroRepository.findById(id);
+		if (possivelLivro.isPresent()) {
+			LivroDetalhes livroDetalhes = LivroDetalhes.convert(possivelLivro.get());
+
+			return ResponseEntity.ok(livroDetalhes);
+		}
+
+		return ResponseEntity.notFound().build();
 	}
 }
---
+++ casadocodigo/src/main/java/com/example/casadocodigo/detalheslivro/LivroDetalhes.java
@@ -0,0 +1,66 @@
+package com.example.casadocodigo.detalheslivro;
+
+import java.math.BigDecimal;
+
+import com.example.casadocodigo.cadastrolivro.Livro;
+
+public class LivroDetalhes {
+
+	private String titulo;
+	private String resumo;
+	private String sumario;
+	private BigDecimal preco;
+	private Integer paginas;
+	private String isbn;
+	private String nomeAutor;
+	private String descricaoAutor;
+
+	public LivroDetalhes(Livro livro) {
+		this.titulo = livro.getTitulo();
+		this.resumo = livro.getResumo();
+		this.sumario = livro.getSumario();
+		this.preco = livro.getPreco();
+		this.paginas = livro.getPaginas();
+		this.isbn = livro.getIsbn();
+		this.nomeAutor = livro.getAutor().getNome();
+		this.descricaoAutor = livro.getAutor().getDescricao();
+
+	}
+
+	public static LivroDetalhes convert(Livro livro) {
+		return new LivroDetalhes(livro);
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public String getResumo() {
+		return resumo;
+	}
+
+	public String getSumario() {
+		return sumario;
+	}
+
+	public BigDecimal getPreco() {
+		return preco;
+	}
+
+	public Integer getPaginas() {
+		return paginas;
+	}
+
+	public String getIsbn() {
+		return isbn;
+	}
+
+	public String getNomeAutor() {
+		return nomeAutor;
+	}
+
+	public String getDescricaoAutor() {
+		return descricaoAutor;
+	}
+
+}
